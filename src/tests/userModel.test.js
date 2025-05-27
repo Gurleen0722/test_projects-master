6d16ae0ac5876577a63a07232ff4fff1679bd197
Gurleen0722 <gurleen.kaurr44@gmail.com>
2025-05-26 19:01:36 -0600

    Update

---
+++ src/tests/userModel.test.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const {User, createUser } = require('../functions/userModel.js');
+
+//Test suite 
+describe('User Model Test', () => {
+    //jest hook 
+    beforeEach(() => {
+        jest.clearAllMocks;
+    });
+
+    it('Should create a new user', () =>{
+
+    });
+});
