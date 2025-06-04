ee4481944784a93a56de3d4ddffb73bfb13d08a9
Gurleen0722 <gurleen.kaurr44@gmail.com>
2025-06-03 18:46:11 -0600

    Update

--- src/tests/userModel.test.js
+++ src/tests/userModel.test.js
@@ -1,29 +1,26 @@
 const mongoose = require('mongoose');
-const {User, createUser } = require('../functions/userModel.js');
+const { User, createUser } = require('../functions/userModel.js');
 
-//Test suite 
 describe('User Model Test', () => {
-    //jest hook 
     beforeEach(() => {
-        jest.clearAllMocks;
+        jest.clearAllMocks(); 
     });
 
-    it('Should create a new user', async () =>{
-       //Arrange 
-       const mockUser = {
-        firstName: 'Gurleen kaur',
-        email: 'cangurleen686@gmail.com',
-        password: 'password',
-        age: 22
-       }
-
-       //Action
-       jest.spyOn(User.prototype, 'save').mockResolvedValue(mockUser);
-       const result = await createUser('Gurleen Kaur', 'cangurleen686@gmail.com', 'password', 22)
-
-       
-       //Assert
-       expect(result).toEqual(expect.objectContaining(mockUser));
-       expect(User.prototype.save()).toHaveBeenCalledTimes(1);
+    it('should create a new user', async () => {
+        // Arrange
+        const mockUser = {
+            age: 100,
+            email: "todd.nash@rdpolytech.ca",
+            firstName: "Todd Nash",
+            password: "password",
+        };
+        
+        // Action
+        jest.spyOn(User.prototype, 'save').mockResolvedValue(mockUser);
+        const result = await createUser('Todd Nash', 'todd.nash@rdpolytech.ca', 'password', 100);
+        
+        // Assert
+        expect(result).toEqual(expect.objectContaining(mockUser));
+        expect(User.prototype.save).toHaveBeenCalledTimes(1); 
     });
 });
